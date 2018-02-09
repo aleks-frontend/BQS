5e3f664bcc3642da2e4de678364d951a02fd165e
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2018-02-09 18:50:54 +0100

    Added geolocation select and datepicker

--- js/scripts.js
+++ js/scripts.js
@@ -247,6 +247,66 @@
     $('#fs-container').height($(window).height() - $('.nav-type-1').height() );
   }
 
+  /* Autocomplete initialization
+  -------------------------------------------------------*/
+
+  function geoLocationAutocomplete() {
+    var autocomplete;
+    var geocoder;
+    var input = document.getElementById('select-location');
+    var options = {
+      componentRestrictions: {'country':'us'},
+      types: ['(regions)'] // (cities)
+    };
+
+    autocomplete = new google.maps.places.Autocomplete(input,options);
+
+    $('#go').click(function(){
+      var location = autocomplete.getPlace();
+      geocoder = new google.maps.Geocoder();
+      console.log(location['geometry'])
+      lat = location['geometry']['location'].lat();
+      lng = location['geometry']['location'].lng();
+      var latlng = new google.maps.LatLng(lat,lng);
+
+      // http://stackoverflow.com/a/5341468
+      geocoder.geocode({'latLng': latlng}, function(results) {
+        for(i=0; i < results.length; i++){
+          for(var j=0;j < results[i].address_components.length; j++){
+            for(var k=0; k < results[i].address_components[j].types.length; k++){
+              if(results[i].address_components[j].types[k] == "postal_code"){
+                zipcode = results[i].address_components[j].short_name;
+                $('span.zip').html(zipcode);    
+              }
+            }
+          }
+        }
+      });
+
+    });  
+  }
+
+  geoLocationAutocomplete();
+  
+  /* Datepicker initialize
+  -------------------------------------------------------*/
+  var today = new Date(),
+      yesterday = new Date(today);
+
+  yesterday.setDate(today.getDate() -1);
+
+  $('.hero-select-date').caleran({
+    singleDate: true,
+    autoCloseOnSelect: true,
+    format: 'MMMM DD, YYYY',
+    disabledRanges: [
+      {
+        start: moment("10/03/1999","MMMM DD, YYYY"),
+        end: moment(yesterday)
+      }    
+    ]
+  });
+
 
   /* Owl Carousel
   -------------------------------------------------------*/
